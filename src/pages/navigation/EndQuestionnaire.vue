0ea6d83f61e340dab0c72697edb8557ede5a6173
marselle13 <nika.gogua8@gmail.com>
2023-05-15 20:02:19 +0400

    style: thank you page style

--- src/pages/navigation/EndQuestionnaire.vue
+++ src/pages/navigation/EndQuestionnaire.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Thank You</h1>
+  <div class="bg-[#232323] grid justify-items-center content-center h-screen">
+    <transition name="thanks" appear>
+      <p class="text-white text-[64px] tracking-[0.24em] font-bold">მადლობა</p>
+    </transition>
+  </div>
 </template>
+<style>
+.thanks-enter-from {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+
+.thanks-enter-active {
+  transition: all 1s ease-out;
+  transition-delay: 1s;
+}
+
+.thanks-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
